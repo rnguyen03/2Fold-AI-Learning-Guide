b6ddf45d923c9b1ce129548be7ab3f29cac17b89
Jason Tan <37030567+AeGGIndie@users.noreply.github.com>
2024-05-18 01:35:41 -0400

    merged

--- hd2/src/app/page.js
+++ hd2/src/app/page.js
@@ -1,43 +1,42 @@
 "use server";
 import { auth, signOut } from "@/lib/auth";
 import Link from "next/link";
-import Navbar from "@/components/navigation";
 import MyDropzone from "@/components/dropFile";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
 
   return (
     <>
-
-
-    
-    <Navbar/>
-    <MyDropzone/>
-    <main className="flex flex-col gapy-y-4 justify-center items-center px-12">
-      <h1>Welcome to the homepage!</h1>
-      <p>User is {!session?.user && "NOT"} logged in</p>
-      <Link className="bg-slate-400 rounded-3xl px-4 py-2" href="/auth/sign-in">
-        Go Login
-      </Link>
-      {session?.user && (
-        <>
-          <form
-            action={async () => {
-              "use server";
-              await signOut();
-            }}
-          >
-            <button className="bg-red-400 rounded-3xl px-4 py-2" type="submit">
-              Sign Out
-            </button>
-          </form>
-        </>
-      )}
-    </main>
+      <main className="flex flex-col gapy-y-4 justify-center items-center px-12">
+        <h1>Welcome to the homepage!</h1>
+        <p>User is {!session?.user && "NOT"} logged in</p>
+        <Link
+          className="bg-slate-400 rounded-3xl px-4 py-2"
+          href="/auth/sign-in"
+        >
+          Go Login
+        </Link>
+        <Link></Link>
+        {session?.user && (
+          <>
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button
+                className="bg-red-400 rounded-3xl px-4 py-2"
+                type="submit"
+              >
+                Sign Out
+              </button>
+            </form>
+          </>
+        )}
+      </main>
     </>
   );
 }
-
-
-
