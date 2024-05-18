bf1d65a4fe1c1a85175e5a6226fb919d6ddac870
Ryan Nguyen <ryanvannguyen@gmail.com>
2024-05-18 03:55:34 -0400

    text editor component added

--- hd2/src/app/page.js
+++ hd2/src/app/page.js
@@ -1,19 +1,20 @@
-"use server";
-import Link from "next/link";
+"use client";
 
+import Link from "next/link";
 import { auth, signOut } from "@/lib/auth";
 import Navbar from "@/components/navigation";
 import ChatGPT from '../components/ChatGPT';
+import SimpleMDEEditor from '../components/SimpleMDEEditor'; // Correct import
 
-export default async function Home() {
-  const session = await auth();
+export default function Home() {
+  // const session = await auth();
 
   return (
     <>
-      <main className="flex flex-col gapy-y-4 justify-center items-center px-12">
+      <main className="flex flex-col gap-y-4 justify-center items-center px-12">
         <h1>Welcome to the homepage!</h1>
-        <p>User is {!session?.user && "NOT"} logged in</p>
         <ChatGPT />
+        <SimpleMDEEditor />
       </main>
     </>
   );
