bf7585e7da333e8c8826775e5d1dcdae80c21181
TonyTran03 <tony.tran03@hotmail.com>
2024-05-19 05:48:54 -0400

    upload sceen includes 5 animals now

--- hd2/src/app/upload/page.js
+++ hd2/src/app/upload/page.js
@@ -13,6 +13,7 @@ const UploadPage = () => {
     const [errorMessage, setErrorMessage] = useState('');
     const fileInputRef = useRef(null);
     const { data: session } = useSession();
+    const [markerDescription, setMarkerDescription] = useState('');
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
@@ -33,11 +34,31 @@ const UploadPage = () => {
         fileInputRef.current.click();
     };
 
+
+
     const handleMarkerClick = (selectedMarker) => {
         setMarker(selectedMarker);
         setErrorMessage('');
+        switch (selectedMarker) {
+            case 'Crane':
+                setMarkerDescription('Crane: Represents your understanding as creative.');
+                break;
+            case 'Ox':
+                setMarkerDescription('Ox: Represents your understanding as foundational.');
+                break;
+            case 'Tiger':
+                setMarkerDescription('Tiger: Represents your understanding as knowledgeable.');
+                break;
+            case 'Rat':
+                setMarkerDescription('Rat: Represents your understanding as courageous.');
+                break;
+            case 'Rabbit':
+                setMarkerDescription('Rabbit: Represents your understanding as fearful.');
+                break;
+            default:
+                setMarkerDescription('');
+        }
     };
-
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -124,7 +145,7 @@ const UploadPage = () => {
         <div className="container mx-auto p-5">
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
                 <h1 className="block text-gray-700 text-xl font-bold mb-2">Upload Your File</h1>
-                <p className="mb-4">Subheading for description or instructions</p>
+                <p className="mb-4">Give it a title and select your companion!</p>
                 <div className="mb-4">
                     <div className="flex items-center space-x-2 mb-3">
                         <button
@@ -153,33 +174,56 @@ const UploadPage = () => {
                     />
                 </div>
                         <div className="flex justify-center mb-4 space-x-6">
-                        <div className="tooltip tooltip-bottom" data-tip="Filler text"  style={{ position: 'relative', display: 'inline-block' }}>
+                        <div className="tooltip tooltip-top" data-tip="Creative"  style={{ position: 'relative', display: 'inline-block' }}>
                         <button 
                                 type="button"
                                 onClick={() => handleMarkerClick('Crane')}
                                 className={`btn btn-circle h-16 w-16 ${marker === 'Crane' ? 'btn-primary' : 'neutral-content'}`}>
-                                <img src="/crane_icon.png" alt="Crane Icon" style={{ width: '70px', height: '70px' }} />
+                                <img src="/crane.png" alt="Crane Icon" style={{ width: '70px', height: '70px' }} />
                         </button>
                     </div>
-                    <div className="tooltip tooltip-bottom" data-tip="Filler text"  style={{ position: 'relative', display: 'inline-block' }}>
+                    <div className="tooltip tooltip-top" data-tip="Foundational"  style={{ position: 'relative', display: 'inline-block' }}>
                     <button
                             type="button"
                             onClick={() => handleMarkerClick('Ox')}
                             className={`btn btn-circle h-16 w-16 ${marker === 'Ox' ? 'btn-primary' : 'neutral-content'}`}>
-                            <img src="/ox_icon.png" alt="ix_icon" style={{ width: '70px', height: '70px' }} />
+                            <img src="/ox.png" alt="ox" style={{ width: '70px', height: '70px' }} />
                     </button>
                     </div>
-                    <div className="tooltip tooltip-bottom" data-tip="Filler text"  style={{ position: 'relative', display: 'inline-block' }}>
+                    <div className="tooltip tooltip-top" data-tip="Knowledgeable"  style={{ position: 'relative', display: 'inline-block' }}>
 
                         <button
                             type="button"
                             onClick={() => handleMarkerClick('Tiger')}
                             className={`btn btn-circle h-16 w-16 ${marker === 'Tiger' ? 'btn-primary' : 'neutral-content'}`}>
-                            <img src="/tiger_icon.png" alt="ix_icon" style={{ width: '70px', height: '70px' }} />
+                            <img src="/tiger.png" alt="ix_icon" style={{ width: '70px', height: '70px' }} />
                         </button>
                     </div>
+
+
+                    <div className="tooltip tooltip-top" data-tip="Courageous"  style={{ position: 'relative', display: 'inline-block' }}>
+
+                        <button
+                            type="button"
+                            onClick={() => handleMarkerClick('Rat')}
+                            className={`btn btn-circle h-16 w-16 ${marker === 'Rat' ? 'btn-primary' : 'neutral-content'}`}>
+                            <img src="/rat.png" alt="rabbit" style={{ width: '70px', height: '70px' }} />
+                        </button>
                     </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+
+
+                    <div className="tooltip tooltip-top" data-tip="Fearful"  style={{ position: 'relative', display: 'inline-block' }}>
+                    <button
+                        type="button"
+                        onClick={() => handleMarkerClick('Rabbit')}
+                        className={`btn btn-circle h-16 w-16 ${marker === 'Rabbit' ? 'btn-primary' : 'neutral-content'}`}>
+                        <img src="/rabbit.png" alt="rabbit" style={{ width: '70px', height: '70px' }} />
+                    </button>
+                    </div>
+
+                    </div>
+                    <p className="text-gray-600 text-sm mt-2 text-center">{markerDescription}</p>
+                <button type="submit" className="btn btn-primary ">Submit</button>
             </form>
         </div>
 
