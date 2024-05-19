9143e026d25d759d8aa40f04d44b9fb66f89efc4
Jason Tan <37030567+AeGGIndie@users.noreply.github.com>
2024-05-19 06:56:48 -0400

    added auth with env variables

--- hd2/src/lib/auth.js
+++ hd2/src/lib/auth.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { app } from "@/app/firebase";
+import { cert } from "firebase-admin/app";
 
 const providers = [
   Google({
@@ -26,5 +27,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   pages: {
     signIn: "/auth/sign-in",
   },
-  adapter: FirestoreAdapter(app),
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY,
+    }),
+  }),
 });
